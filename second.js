03f440f6e6ff7442b8a7183d10ee1a1eb935a5af
Nick <nikita.timohin@quardex.com>
2019-06-07 15:13:54 +0300

    added functions

---
+++ second.js
@@ -0,0 +1,42 @@
+function write1() {
+    document.getElementById('input').value = document.getElementById('input').value + 1;
+}
+function write2() {
+    document.getElementById('input').value = document.getElementById('input').value + 2;
+}
+
+function write3() {
+    document.getElementById('input').value = document.getElementById('input').value + 3;
+}
+
+function write4() {
+    document.getElementById('input').value = document.getElementById('input').value + 4;
+}
+
+function write5() {
+    document.getElementById('input').value = document.getElementById('input').value + 5;
+}
+
+function write6() {
+    document.getElementById('input').value = document.getElementById('input').value + 6;
+}
+
+function write7() {
+    document.getElementById('input').value = document.getElementById('input').value + 7;
+}
+
+function write8() {
+    document.getElementById('input').value = document.getElementById('input').value + 8;
+}
+
+function write9() {
+    document.getElementById('input').value = document.getElementById('input').value + 9;
+}
+
+function write0() {
+    document.getElementById('input').value = document.getElementById('input').value + 0;
+}
+
+function writeCE() {
+    document.getElementById('input').value = 0;
+}
