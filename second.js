e17b56ad38bacc6d89cacd5e1229fb7f2213c616
Ozzeron <40788047+Ozzeron@users.noreply.github.com>
2019-06-12 23:33:16 +0300

    Update second.js

--- second.js
+++ second.js
@@ -5,16 +5,16 @@ let glob2 = 0;
 function write1(a) {
     switch(a){
         case 1:
-            document.getElementById('input').value = document.getElementById('input').value + 1;
+            document.getElementById('input').value += 1;
             break;
         case 2:
-            document.getElementById('input').value = document.getElementById('input').value + 2;
+            document.getElementById('input').value += 2;
             break;
         case 3:
-            document.getElementById('input').value = document.getElementById('input').value + 3;
+            document.getElementById('input').value += 3;
             break;
         case 4:
-            document.getElementById('input').value = document.getElementById('input').value + 4;
+            document.getElementById('input').value += 4;
             break;
         case 'ce':
             document.getElementById('input').value = 0;
