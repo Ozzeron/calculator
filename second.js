cad48565dfa930d29767d25471e96b6b39373ae4
Nick <nikita.timohin@quardex.com>
2019-06-12 21:31:43 +0300

    Revert "New logic"
    This reverts commit 2f6a005

--- second.js
+++ second.js
@@ -2,64 +2,22 @@ let glob1 = 0;
 let glob2 = 0;
 
 
-function write1() {
-    document.getElementById('input').value = document.getElementById('input').value + 1;
+function write1(a) {
+    switch(a){
+        case 1:
+            document.getElementById('input').value = document.getElementById('input').value + 1;
+            break;
+        case 2:
+            document.getElementById('input').value = document.getElementById('input').value + 2;
+            break;
+        case 3:
+            document.getElementById('input').value = document.getElementById('input').value + 3;
+            break;
+        case 4:
+            document.getElementById('input').value = document.getElementById('input').value + 4;
+            break;
+        case 'ce':
+            document.getElementById('input').value = 0;
+            break;
+    }
 }
-function write2() {
-    document.getElementById('input').value = document.getElementById('input').value + 2;
-}
-
-function write3() {
-    document.getElementById('input').value = document.getElementById('input').value + 3;
-}
-
-function write4() {
-    document.getElementById('input').value = document.getElementById('input').value + 4;
-}
-
-function write5() {
-    document.getElementById('input').value = document.getElementById('input').value + 5;
-}
-
-function write6() {
-    document.getElementById('input').value = document.getElementById('input').value + 6;
-}
-
-function write7() {
-    document.getElementById('input').value = document.getElementById('input').value + 7;
-}
-
-function write8() {
-    document.getElementById('input').value = document.getElementById('input').value + 8;
-}
-
-function write9() {
-    document.getElementById('input').value = document.getElementById('input').value + 9;
-}
-
-function write0() {
-    document.getElementById('input').value = document.getElementById('input').value + 0;
-}
-
-function writeCE() {
-    document.getElementById('input').value = 0;
-}
-function writeEqual() {
-    document.getElementById('input').value = eval(document.getElementById('input').value);
-}
-function writeMinus() {
-    document.getElementById('input').value = document.getElementById('input').value + '-';
-}
-function writeplus() {
-    document.getElementById('input').value = document.getElementById('input').value + '+';
-}
-function writeMulti() {
-    document.getElementById('input').value =  document.getElementById('input').value + '*';
-}
-function writeSlash() {
-    document.getElementById('input').value = document.getElementById('input').value + '/';
-}
-// function writeSqrt() {
-//     document.getElementById('input').value = document.getElementById('input').value + &#8730;
-// }
-
