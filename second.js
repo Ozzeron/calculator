599f35f8ffd844130422822a84c717ff372fd203
Nick <nikita.timohin@quardex.com>
2019-06-13 10:20:59 +0300

    optimize js

--- second.js
+++ second.js
@@ -3,21 +3,7 @@ let glob2 = 0;
 
 
 function write1(a) {
-    switch(a){
-        case 1:
-            document.getElementById('input').value += 1;
-            break;
-        case 2:
-            document.getElementById('input').value += 2;
-            break;
-        case 3:
-            document.getElementById('input').value += 3;
-            break;
-        case 4:
-            document.getElementById('input').value += 4;
-            break;
-        case 'ce':
-            document.getElementById('input').value = 0;
-            break;
-    }
+    document.getElementById('input') += a;
 }
+
+
