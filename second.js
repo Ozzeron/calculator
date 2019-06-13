68dd3afc6d48607906c02edaf4cdb8c59fe628ce
Nick <nikita.timohin@quardex.com>
2019-06-13 15:33:46 +0300

    optimize js

--- second.js
+++ second.js
@@ -6,4 +6,17 @@ function write1(a) {
     document.getElementById('input').value += a;
 }
 
+function doMath(a) {
+    switch (a) {
+        case 'giveResult':
+            document.getElementById('input').value = eval(document.getElementById('input').value);
+            break;
+        case 'ce':
+            document.getElementById('input').value = '';
+            break;
+        case 'removeOne':
+            document.getElementById('input').value = document.getElementById('input').value[document.getElementById('input').value.length-1];
+            break;
+    }
+}
 
