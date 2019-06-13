64d019d245a006cd2e10c7b7e0e0b572d3ca42ed
Nick <nikita.timohin@quardex.com>
2019-06-13 17:12:21 +0300

    optimize js

--- second.js
+++ second.js
@@ -9,6 +9,7 @@ function write1(a) {
 function doMath(a) {
     switch (a) {
         case 'giveResult':
+            glob1 = document.getElementById('input').value;
             document.getElementById('input').value = eval(document.getElementById('input').value);
             break;
         case 'ce':
@@ -17,12 +18,19 @@ function doMath(a) {
         case 'removeOne':
             document.getElementById('input').value = document.getElementById('input').value.substring(0, document.getElementById('input').value.length-1)
             break;
+        case 'revert':
+            document.getElementById('input').value = glob1;
+            break;
     }
 }
+
+//проверка на нажатие клавиш + блок ввода любых букв
 document.onkeypress = function(event){
     if (event.keyCode === 13){
         doMath('giveResult');
     }
+    else if (event.charCode && (event.charCode < 48 || event.charCode > 57))// проверка на event.charCode - чтобы пользователь мог нажать backspace, enter, стрелочку назад...
+    return false;
 }
 
 
